2dda6f2351a0f4454dadfc2b3ec5036f897067ff
Bakhodir <tba-93@yandex.com>
2021-09-17 11:49:12 +0500

    Company REST - first commit

---
+++ src/main/java/com/example/task2_1_1/Task211Application.java
@@ -0,0 +1,13 @@
+package com.example.task2_1_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task211Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Task211Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/task2_1_1/Task211ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task2_1_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task211ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
