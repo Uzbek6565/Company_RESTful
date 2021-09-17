1e1f56916ccce573f66f169d848fa070f034464d
Bakhodir <tba-93@yandex.com>
2021-09-17 16:55:15 +0500

    CompanyController CRUD is started

--- src/main/java/com/example/task2_1_1/controller/CompanyController.java
+++ src/main/java/com/example/task2_1_1/controller/CompanyController.java
@@ -1,8 +1,10 @@
 package com.example.task2_1_1.controller;
 
+import com.example.task2_1_1.payload.CompanyDto;
 import com.example.task2_1_1.payload.ResponseApi;
 import com.example.task2_1_1.service.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +17,9 @@ public class CompanyController {
     @Autowired
     CompanyService companyService;
 
-    //@PostMapping
-    //public ResponseEntity<ResponseApi> create(@RequestBody)
+    @PostMapping
+    public ResponseEntity<ResponseApi> create(@RequestBody CompanyDto companyDto){
+        ResponseApi responseApi = companyService.create(companyDto);
+        return ResponseEntity.status(responseApi.isSuccess()? HttpStatus.CREATED : HttpStatus.BAD_REQUEST).body(responseApi);
+    }
 }
--- src/main/java/com/example/task2_1_1/service/CompanyService.java
+++ src/main/java/com/example/task2_1_1/service/CompanyService.java
@@ -1,7 +1,21 @@
 package com.example.task2_1_1.service;
 
+import com.example.task2_1_1.payload.CompanyDto;
+import com.example.task2_1_1.payload.ResponseApi;
+import com.example.task2_1_1.repository.AddressRepository;
+import com.example.task2_1_1.repository.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CompanyService {
+    @Autowired
+    CompanyRepository companyRepository;
+
+    @Autowired
+    AddressRepository addressRepository;
+
+    public ResponseApi create(CompanyDto companyDto) {
+
+    }
 }
